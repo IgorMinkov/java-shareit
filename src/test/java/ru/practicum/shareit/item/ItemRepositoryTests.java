d4e98e78b0c33a92d7bb141e3c6e0da00b7d2d3a
igor.minkov <igor.minkov@rt.ru>
2024-02-25 14:46:12 +0300

    15.3 - add ItemController and ItemRepository tests

--- src/test/java/ru/practicum/shareit/item/ItemRepositoryTests.java
+++ src/test/java/ru/practicum/shareit/item/ItemRepositoryTests.java
@@ -1,4 +1,78 @@
 package ru.practicum.shareit.item;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.PageRequest;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.item.repository.ItemRepository;
+import ru.practicum.shareit.user.User;
+import ru.practicum.shareit.user.repository.UserRepository;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DataJpaTest
 public class ItemRepositoryTests {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    Item firstitem;
+    Item secondItem;
+    User user;
+
+    @BeforeEach
+    void setUp() {
+
+        user = User.builder()
+                .name("Alex")
+                .email("alexFirst@yandex.ru")
+                .build();
+        userRepository.save(user);
+
+        firstitem = Item.builder()
+                .name("itemName")
+                .description("item description")
+                .available(true)
+                .owner(user)
+                .build();
+        itemRepository.save(firstitem);
+
+        secondItem = Item.builder()
+                .name("item")
+                .description("description for search test")
+                .available(true)
+                .owner(user)
+                .build();
+        itemRepository.save(secondItem);
+    }
+
+    @Test
+    void search() {
+
+        List<Item> descriptionSearchResult = itemRepository.search("search", PageRequest.of(0, 1))
+                .toList();
+
+        assertEquals(1, descriptionSearchResult.size());
+        assertEquals("item", descriptionSearchResult.get(0).getName());
+
+        List<Item> nameSearchResult = itemRepository.search("item", PageRequest.of(0, 2))
+                .toList();
+        assertEquals(2, nameSearchResult.size());
+        assertEquals("item", descriptionSearchResult.get(0).getName());
+    }
+
+    @AfterEach
+    void tearDown() {
+        userRepository.deleteAll();
+        itemRepository.deleteAll();
+    }
+
 }
