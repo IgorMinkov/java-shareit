244ed9efa4c776041447df4ed25ce7e7beba5aac
igor.minkov <igor.minkov@rt.ru>
2024-03-03 00:21:55 +0300

    16.2 - refactor server controllers and input DTO at User and ItemRequest classes

--- server/src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ server/src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -1,13 +1,22 @@
 package ru.practicum.shareit.request.dto;
 
+import lombok.Builder;
 import lombok.Data;
+import ru.practicum.shareit.item.dto.ItemDto;
 
-import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
+@Builder
 public class ItemRequestDto {
 
-    @NotBlank
+    private Long id;
+
     private String description;
+
+    private LocalDateTime created;
+
+    private List<ItemDto> items;
 
 }
--- server/src/main/java/ru/practicum/shareit/user/UserController.java
+++ server/src/main/java/ru/practicum/shareit/user/UserController.java
@@ -2,19 +2,16 @@ package ru.practicum.shareit.user;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.validation.annotation.Validated;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.dto.UserMapper;
 import ru.practicum.shareit.user.service.UserService;
 
-import javax.validation.Valid;
-import javax.validation.constraints.Positive;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Slf4j
-@Validated
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
@@ -23,39 +20,40 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping
-    public UserDto addUser(@Valid @RequestBody UserDto userDto) {
+    public ResponseEntity<UserDto> addUser(@RequestBody UserDto userDto) {
         User user = userService.create(UserMapper.toUser(userDto));
         log.info("Добавлен пользователь: {} ", user);
-        return UserMapper.toUserDto(user);
+        return ResponseEntity.ok(UserMapper.toUserDto(user));
     }
 
     @PatchMapping("/{userId}")
-    public UserDto updateUser(
+    public ResponseEntity<UserDto> updateUser(
             @RequestBody UserDto userDto,
-            @Positive @PathVariable Long userId) {
+            @PathVariable Long userId) {
         User updatedUser = userService.update(UserMapper.toUser(userDto), userId);
         log.info("Обновлен пользователь: {} ", updatedUser);
-        return UserMapper.toUserDto(updatedUser);
+        return ResponseEntity.ok(UserMapper.toUserDto(updatedUser));
     }
 
     @DeleteMapping("/{userId}")
-    public void deleteUser(@Positive @PathVariable Long userId) {
+    public ResponseEntity<Void> deleteUser(@PathVariable Long userId) {
         userService.delete(userId);
         log.info("Пользователь с id: {} удален", userId);
+        return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/{userId}")
-    public UserDto getUser(@Positive @PathVariable Long userId) {
+    public ResponseEntity<UserDto> getUser(@PathVariable Long userId) {
         User user = userService.getById(userId);
         log.info("Найден пользователь по id: {} ", user);
-        return UserMapper.toUserDto(user);
+        return ResponseEntity.ok(UserMapper.toUserDto(user));
     }
 
     @GetMapping
-    public List<UserDto> getAllUsers() {
-        return userService.getAll().stream()
+    public ResponseEntity<List<UserDto>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAll().stream()
                 .map(UserMapper::toUserDto)
-                .collect(Collectors.toList());
+                .collect(Collectors.toList()));
     }
 
 }
